b2f011603ce820eee66be61cbab16a95248b095b
Roberta Huber <roberta.huber@bistudio.com>
2022-04-16 08:30:43 +0200

    unused import

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,6 +1,4 @@
-import React from 'react';
-
-function Footer(props) {
+function Footer() {
   const footerYear = new Date().getFullYear;
   return (
     <footer className="footer p-10 bg-gray-700 text-primary-content footer-center">
